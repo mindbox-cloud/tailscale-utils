0d11e8d69c91582d09ed6b025b2b11580ceaede5
WGOS <thewgosplayer@gmail.com>
2023-11-23 15:40:06 +0400

    Make ps1 use args

--- tailscale-setup.ps1
+++ tailscale-setup.ps1
@@ -6,26 +6,24 @@ if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
     exit
 }
 
-function Get-VarOrDefault {
-    param(
-        $VarName,
-        $DefVal
-    )
+param(
+    [string]$TsLoginServer = "https://controlplane.tailscale.com"
+    [string]$TsAuthKey
+    [string]$TsHostname = [System.Net.Dns]::GetHostName()
+    [string]$TsPkgsDomain = "pkgs.tailscale.com"
+    [string]$TsUpArgs
 
-    $envVarValue = [System.Environment]::GetEnvironmentVariable($VarName)
+    [string[]]$TsAdvertiseRoutes
+    [string[]]$TsTags
 
-    if ([string]::IsNullOrWhiteSpace($envVarValue)) {
-        [System.Environment]::SetEnvironmentVariable($VarName, $DefVal)
-        return $DefVal
-    } else {
-        return $envVarValue
-    }
-}
+    [bool]$TsUnattended = $true
+    [bool]$TsAcceptDns = $true
+    [bool]$TsAcceptRoutes = $true
+    [bool]$TsAutoUpdate = $true
+    [bool]$TsUpSkip = $false
+)
 
-$env:TS_DL_URL = Get-VarOrDefault "TS_DL_URL" "https://pkgs.tailscale.com/stable/tailscale-setup-latest-amd64.msi"
-$env:TS_UNATTENDED = Get-VarOrDefault "TS_UNATTENDED" "true"
-$env:TS_ACCEPT_DNS = Get-VarOrDefault "TS_ACCEPT_DNS" "true"
-$env:TS_ACCEPT_ROUTES = Get-VarOrDefault "TS_ACCEPT_ROUTES" "true"
+$TsDlUrl = "https://$TsPkgsDomain/stable/tailscale-setup-latest-amd64.msi"
 
 $tempFolder = [System.IO.Path]::GetTempPath()
 $fileName = "ts_setup.msi"
@@ -35,30 +33,42 @@ $ErrorActionPreference = "Stop"
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
 
 echo "Downloading Tailscale setup package to `"$destinationPath`""
-Invoke-WebRequest -Uri $env:TS_DL_URL -OutFile $destinationPath
+Invoke-WebRequest -Uri $TsDlUrl -OutFile $destinationPath
 
+# Probably should be allowed to configure via args
 $args = "/i `"$destinationPath`" /qn"
 $args += " TS_UNATTENDEDMODE=always"
 $args += " TS_ALLOWINCOMINGCONNECTIONS=always"
 
 echo "Installing Tailscale"
 Start-Process -FilePath "msiexec.exe" -ArgumentList $args -Wait
-
 Remove-Item -Path "$destinationPath" -Confirm:$false -Force
 
 # We shall reload PATH to set up tailscale further
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine")
 
-$args = "up"
-$args += " --login-server $env:TS_LOGIN_SERVER"
-$args += " --auth-key $env:TS_AUTH_TOKEN"
-$args += " --hostname $env:TS_HOSTNAME" 
-$args += " --unattended=$env:TS_UNATTENDED"
-$args += " --accept-dns=$env:TS_ACCEPT_DNS"
-$args += " --accept-routes=$env:TS_ACCEPT_ROUTES"
+$args = "up $TsUpArgs"
+$args += " --login-server $TsLoginServer"
+$args += " --hostname $TsHostname" 
+$args += " --unattended=$TsUnattended"
+$args += " --accept-dns=$TsAcceptDns"
+$args += " --accept-routes=$TsAcceptRoutes"
 
-if (![string]::IsNullOrWhiteSpace($env:TS_TAGS)) {
-    $args += " --advertise-tags $env:TS_TAGS"
+if (![string]::IsNullOrWhiteSpace($TsAuthKey)) {
+    $args += " --auth-key $TsAuthKey"
+}
+
+if ($TsTags.Length -gt 0) {
+    $tagsArr = $TsTags | ForEach-Object { "tag:$_" }
+    $tags = $tagsArr -join ','
+
+    $args += " --advertise-tags $tags"
+}
+
+if($TsAdvertiseRoutes.Length -gt 0) {
+    $advRoutes = $TsAdvertiseRoutes -join ','
+
+    $args += " --advertise-routes $advRoutes"
 }
 
 echo "Let me sleep for 10 seconds before continuing to ensure that Tailscale service has started"
@@ -68,7 +78,5 @@ echo "Joining Tailnet"
 Start-Process -FilePath "tailscale.exe" -ArgumentList $args -Wait
 
 $args = "set"
-$args += " --auto-update=true"
-
-echo "Enabling auto updates"
+$args += " --auto-update=$TsAutoUpdate"
 Start-Process -FilePath "tailscale.exe" -ArgumentList $args -Wait
